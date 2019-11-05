c39b451a35558f3baab061210d13b5abe1831f26
Idriss Laouali <idrisslaouali@gmail.com>
2019-11-05 17:43:38 +0100

    part7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { ProductListComponent } from './product-list/product-list.component'
 const appRoutes: Routes = [
   {path: 'products', component: ProductListComponent},
-  {path: 'products/:id', component: ProductPageComponent},
+  {path: 'product/:id', component: ProductPageComponent},
   {path: '', redirectTo: 'products', pathMatch: 'full'},
 
 ];
